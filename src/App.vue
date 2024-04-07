6d8d97ccd00a4fb832e5f719f324653575bd24f6
Omar Aguirre <omar.fi.wwr@gmail.com>
2024-04-07 08:43:22 -0600

    Added vue router vue to the project

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,13 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <b-row>
-      <b-col col="3">1</b-col>
-      <b-col col="3">2</b-col>
-      <b-col col="3">3</b-col>
-      <b-col col="3">4</b-col>
-    </b-row>
-    <div>
-      <b-container>
-        <b-row>
-          <b-col cols="6">
-            <div class="bg-primary text-white p-3">Columna 1</div>
-            <b-button variant="primary" class="mt-1 mb-1">Botón Primario</b-button>
-          </b-col>
-          <b-col cols="6">
-            <div class="bg-secondary text-white p-3">Columna 2</div>
-            <b-button variant="secondary" class="mt-1 mb-1">Botón Secundario</b-button>
-          </b-col>
-        </b-row>
-      </b-container>
-    </div>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <b-container class="mt-4">
+      <b-row>
+        <b-col cols="12" class="text-center">
+          <h1>Bienvenido a la Página de Inicio</h1>
+          <p>¡Haz clic en el botón para ir a la página "About"!</p>
+          <b-button @click="redirectToAbout" variant="primary">Ir a About</b-button>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    redirectToAbout() {
+      this.$router.push({ name: 'About' });
+    }
+  }
+}
+</script>
