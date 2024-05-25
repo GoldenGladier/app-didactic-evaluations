3a0b6f6bf698b05f4f15b5823ed59ef90997f62d
Omar Aguirre <omar.fi.wwr@gmail.com>
2024-05-24 18:54:51 -0600

    Redesigned home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,64 +1,223 @@
 <template>
-  <div class="home-container custom-card-box">
-    <b-container class="pt-4">
-      <div>
-        <b-carousel
-          id="carousel-no-animation"
-          style="text-shadow: 0px 0px 2px #000"
-          no-animation
-          indicators
-          img-width="1024"
-          img-height="480"
-        >
-          <b-carousel-slide
-            caption="First Slide"
-            img-src="https://picsum.photos/1024/480/?image=10"
-          ></b-carousel-slide>
-          <b-carousel-slide
-            caption="Second Slide"
-            img-src="https://picsum.photos/1024/480/?image=12"
-          ></b-carousel-slide>
-          <b-carousel-slide
-            caption="Third Slide"
-            img-src="https://picsum.photos/1024/480/?image=22"
-          ></b-carousel-slide>
-          <b-carousel-slide
-            caption="Fourth Slide"
-            img-src="https://picsum.photos/1024/480/?image=23"
-          ></b-carousel-slide>
-        </b-carousel>
-      </div>      
-      <b-row class="pt-4">
-        <b-col cols="12" class="text-center">
-          <h1>Bienvenido a la Página de Inicio</h1>
-          <p>¡Haz clic en el botón para ir a la página "About"!</p>
-          <b-button @click="redirectToAbout" variant="primary">Ir a About</b-button>
+  <b-container class="custom-master-container custom-card-box custom-center-flex">
+    <b-container>
+      <!-- Header Section -->
 
-          <b-dropdown id="dropdown-dropleft" offset="-100" text="Drop-Left" variant="primary" class="ml-1">
-            <b-dropdown-item href="#">Action</b-dropdown-item>
-            <b-dropdown-item href="#">Another action</b-dropdown-item>
-            <b-dropdown-item href="#">Something else here</b-dropdown-item>
-          </b-dropdown>          
+      <!-- Banner Section -->
+      <b-row class="banner-section my-5 d-flex justify-content-center align-items-center">
+        <b-col cols="12" md="6">
+          <div class="banner-text">
+            <h1 class="display-3 font-weight-bold">{{ bannerText }}</h1>
+            <p class="lead mt-4">{{ bannerDescription }}</p>
+          </div>
+          <div class="my-4">
+            <!-- <b-button type="submit" variant="primary" class="mr-2"><i class="bi bi-arrow-right"></i>Comenzar</b-button> -->
+
+            <router-link v-if="!isLoggedIn" to="/register" class="btn btn-primary mr-2">
+              <i class="bi bi-person-fill-add"></i> Registrarse
+            </router-link>
+
+            <div v-else>
+              <router-link to="/register" class="btn btn-primary mr-2">
+                <i class="bi bi-plus-lg"></i>Crear evaluación
+              </router-link>
+              <router-link to="/register" class="btn btn-outline-primary mr-2">
+                <i class="bi bi-card-checklist"></i>Ver mis evaluaciones
+              </router-link>              
+            </div>
+            <!-- <b-button type="submit" variant="primary" class="mr-2"><i class="bi bi-plus-lg"></i>Crear evaluación</b-button>
+            <b-button type="submit" variant="outline-primary" class="ml-2"><i class="bi bi-card-checklist"></i>Ver mis evaluaciones</b-button> -->
+
+
+            <!-- <b-button type="submit" variant="primary" class="mr-2"><i class="bi bi-plus-lg"></i>Crear evaluación</b-button> -->
+            <!-- <b-button type="submit" variant="outline-primary" class="ml-2" @click="logout()"><i class="bi bi-person-fill-x"></i>No soy yo</b-button> -->
+          </div>
+        </b-col>
+        <b-col cols="12" md="6">
+          <img src="@/assets/online_test.svg" alt="Banner Image" class="img-fluid">
+        </b-col>     
+      </b-row>
+
+
+      <!-- <b-row class="text-center my-5">
+        <b-col>
+          <h1 class="display-3 font-weight-bold">Bienvenido a EvaluApp</h1>
+          <p class="lead">Transforma la manera en que evalúas a tus estudiantes con herramientas interactivas y dinámicas.</p>
+        </b-col>
+      </b-row> -->
+
+      <!-- Features Section -->
+      <b-row class="text-center my-5">
+        <b-col cols="12" class="mb-4">
+          <h2 class="display-4 font-weight-bold ">Instrumentos de Evaluación</h2>
+          <p class="lead">Descubre la variedad de actividades de evaluación que EvaluApp ofrece. Desde clasificación hasta resolución de crucigramas, nuestras herramientas están diseñadas para fomentar el aprendizaje interactivo y el pensamiento crítico. Explora nuestras actividades personalizables y lleva la evaluación al siguiente nivel con EvaluApp.</p>
+        </b-col>
+
+        <b-col md="6" lg="3" class="mb-4" v-for="(activity, index) in activities" :key="index">
+          <b-card :title="activity.title" class="text-center">
+            <b-icon :icon="activity.icon" font-scale="4" class="my-3"></b-icon>
+            <b-card-text>{{ activity.description }}</b-card-text>
+            <!-- <b-button variant="primary" @click="selectActivity(activity.type)">Crear {{ activity.title }}</b-button> -->
+          </b-card>
         </b-col>
       </b-row>
+
+
+      <!-- Share Section -->
+      <b-row class="banner-section my-5 d-flex justify-content-center align-items-center">
+        <b-col cols="12" md="5">
+          <img src="@/assets/share_link.svg" alt="Share Link Or Pin" class="img-fluid">
+        </b-col>        
+        <b-col cols="12" md="7">
+          <div class="banner-text my-4">
+            <h2 class="font-weight-bold">Comparte tus evaluaciones fácilmente</h2>
+            <p class="lead">Con EvaluApp, compartir tus evaluaciones es simple y seguro. Generamos un PIN único para cada actividad, asegurando su exclusividad. Además, puedes enviar el enlace directo a tus alumnos, facilitando su acceso con un solo clic. Simplifica la distribución y acceso a tus evaluaciones con EvaluApp.</p>            
+          </div>
+        </b-col>     
+      </b-row>      
+
+      <!-- Response Settings Section -->
+      <b-row class="banner-section my-5 d-flex justify-content-center align-items-center">
+        <b-col cols="12" md="7">
+          <div class="banner-text my-4">
+            <h2 class="font-weight-bold">Configuraciones de Respuesta Personalizadas</h2>
+            <p class="lead">
+              En EvaluApp, tú decides cómo los alumnos reciben retroalimentación al entregar sus respuestas. Puedes optar por mostrarles las respuestas correctas al instante, sin permitirles corregir sus respuestas previas. Esta opción fomenta un aprendizaje activo y la corrección inmediata de errores. Alternativamente, puedes elegir que los alumnos envíen sus respuestas sin ver ninguna retroalimentación inmediata, promoviendo un enfoque más reflexivo y desafiante. Tú tienes el control total sobre la experiencia de evaluación.            
+            </p>
+          </div>
+        </b-col>         
+        <b-col cols="12" md="5">
+          <img src="@/assets/test_approve.svg" alt="Custom Feedback Settings" class="img-fluid">
+        </b-col>            
+      </b-row>     
+
+      <!-- Grading and Review Section -->
+      <b-row class="banner-section my-5 d-flex justify-content-center align-items-center">
+        <b-col cols="12" md="5">
+          <img src="@/assets/test_results.svg" alt="Share Link Or Pin" class="img-fluid">
+        </b-col>        
+        <b-col cols="12" md="7">
+          <div class="banner-text my-4">
+            <h2 class="font-weight-bold">Revisión de Calificaciones</h2>
+            <p class="lead">
+              Accede a las calificaciones y respuestas de cada alumno para una evaluación detallada. Esta función te permite a ti mismo ver el rendimiento individual, identificar áreas de mejora y proporcionar retroalimentación específica en tu aula. Con un análisis claro, puedes adaptar tus estrategias de enseñanza para satisfacer las necesidades de cada alumno, mejorando el aprendizaje en tu aula.
+            </p>
+          </div>
+        </b-col>     
+      </b-row>   
+
+      <!-- Multiplatform Access  -->
+      <b-row class="banner-section my-5 d-flex justify-content-center align-items-center">
+        <b-col cols="12" md="7">
+          <div class="banner-text my-4">
+            <h2 class="font-weight-bold">Acceso Multiplataforma</h2>
+            <p class="lead">
+              EvaluApp ofrece acceso multiplataforma para una experiencia sin complicaciones. Tanto tú como tus alumnos pueden acceder a la plataforma desde cualquier dispositivo con conexión a internet: ya sea un celular, una computadora o una tableta. No es necesario instalar ninguna aplicación adicional; simplemente abre tu navegador web favorito y accede a EvaluApp. Esto asegura una experiencia fluida y conveniente, independientemente de dónde te encuentres o qué dispositivo estés utilizando.
+            </p>
+          </div>
+        </b-col>         
+        <b-col cols="12" md="5">
+          <img src="@/assets/multi_devices.svg" alt="Multiplatform Access" class="img-fluid">
+        </b-col>            
+      </b-row>         
     </b-container>
-  </div>
+  </b-container>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      bannerText: '',
+      bannerDescription: 'Transforma la manera en que evalúas a tus estudiantes con herramientas interactivas y dinámicas.',  
+      activities: [
+        {
+          title: 'Ordena los Ítems',
+          description: 'Crea una evaluación donde los alumnos deben ordenar ítems correctamente.',
+          icon: 'sort-alpha-down',
+          type: 'sort-items'
+        },
+        {
+          title: 'Ordena el Enunciado',
+          description: 'Desafía a tus alumnos a ordenar enunciados de forma correcta.',
+          icon: 'sort-alpha-down-alt',
+          type: 'sort-sentence'
+        },
+        {
+          title: 'Sopa de Letras',
+          description: 'Genera sopas de letras para que tus alumnos encuentren las palabras escondidas.',
+          icon: 'grid-3x3-gap-fill',
+          type: 'word-search'
+        },
+        {
+          title: 'Crucigramas',
+          description: 'Diseña crucigramas educativos para tus alumnos.',
+          icon: 'journal',
+          type: 'crossword'
+        }
+      ]
+    };
+  },
+  mounted() {
+    // Text animation effect
+    this.animateText();
+  },  
   methods: {
-    redirectToAbout() {
-      this.$router.push({ name: 'About' });
+    // Function to animate text
+    animateText() {
+      const text = 'Bienvenido a EvaluApp';
+      let index = 0;
+      const intervalId = setInterval(() => {
+        if (index <= text.length) {
+          this.bannerText = text.slice(0, index);
+          index++;
+        } else {
+          clearInterval(intervalId);
+        }
+      }, 100);
+    },    
+    selectActivity(type) {
+      // Navegar a la página de creación de actividad correspondiente
+      this.$router.push({ name: 'CreateActivity', params: { activityType: type } });
     }
   }
-}
+};
 </script>
 
 <style scoped>
-.home-container {
-  position: absolute;
-  width: 100%;
-  height: 100%;
+.homepage {
+  background-color: #f8f9fa;
+}
+
+h1, h2 {
+  color: #343a40;
+}
+
+b-card {
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  transition: transform 0.2s;
+}
+
+b-card:hover {
+  transform: translateY(-5px);
+}
+
+b-button {
+  transition: background-color 0.2s, transform 0.2s;
+}
+
+b-button:hover {
+  background-color: #0056b3;
+  transform: translateY(-2px);
+}
+
+.my-5 {
+  margin-top: 3rem !important;
+  margin-bottom: 3rem !important;
+}
+
+.my-4 {
+  margin-top: 2rem !important;
+  margin-bottom: 2rem !important;
 }
 </style>
